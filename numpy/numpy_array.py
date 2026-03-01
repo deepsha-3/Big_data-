5e5fa0a45ab55fa87c3fb43e07779f2d9fe34a1f
deepsha-3 <itsmedeepsha375@gmail.com>
2026-03-01 21:48:09 +0545

    common NumPy array creation methods

--- numpy/numpy_array.py
+++ numpy/numpy_array.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 # Different ways to create arrays
-arr1 = np.array([1, 2, 3, 4, 5])  # From list
-arr2 = np.zeros((3, 4))  # 3x4 array of zeros
+arr1 = np.array([1, 2, 3, 4, 5])  
+arr2 = np.zeros((3, 4))   # 3x4 array of zeros
 arr3 = np.ones((2, 3))  # 2x3 array of ones
 arr4 = np.arange(10)  # 0 to 9
 arr5 = np.linspace(0, 10, 5)  # 5 equally spaced numbers from 0 to 10
