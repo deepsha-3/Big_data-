afa45d98e1415d44479f6ba3cad9639280af2f47
deepsha-3 <itsmedeepsha375@gmail.com>
2026-03-02 22:24:30 +0545

    dataframe operations using Pandas

--- pandas/dataframe_operation.py
+++ pandas/dataframe_operation.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.DataFrame({
     'Name': ['Riya', 'Diya', 'Max', 'Tina', 'Priya'],
     'Age': [25, 30, 35, 28, 32],
-    'City': ['New York', 'London', 'Paris', 'Tokyo', 'Sydney'],
+    'City': ['New York', 'London', 'Paris', 'Pokhara', 'Sydney'],
     'Salary': [50000, 60000, 75000, 55000, 68000],
     'Department': ['HR', 'IT', 'Finance', 'IT', 'HR']
 })
